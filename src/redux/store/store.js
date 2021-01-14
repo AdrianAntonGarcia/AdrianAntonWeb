c533c6b6775f7da78137457e66b0c77989f54cd8
adriton <adrtler@gmail.com>
2021-01-14 19:13:02 +0100

    Ejemplo de efecto redux

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -9,5 +9,21 @@ const composeEnhancers =
 
 export const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk, logger))
 );
+
+// Ejemplo de efecto
+function logger({ getState }) {
+  return (next) => (action) => {
+    console.log('will dispatch', action);
+
+    // Call the next dispatch method in the middleware chain.
+    const returnValue = next(action);
+
+    console.log('state after dispatch', getState());
+
+    // This will likely be the action itself, unless
+    // a middleware further in chain changed it.
+    return returnValue;
+  };
+}
