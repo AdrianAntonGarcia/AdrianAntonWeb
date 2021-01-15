c5c602a1fc8f83a6b4d796f9f06bb3ca10f46e9b
adriton <adrtler@gmail.com>
2021-01-15 19:11:58 +0100

    Fetch con token y query params

--- src/helpers/services/fetch.js
+++ src/helpers/services/fetch.js
@@ -23,7 +23,7 @@ const fetchSinToken = (endpoint, data, method = 'GET') => {
 };
 
 /**
- *
+ * Función para llamar a los servicios con los query params
  * @param {*} endpoint
  * @param {*} data
  * @param {*} options ej {queryParams:{token:'00213', param: 'sdadas'}}
@@ -51,12 +51,6 @@ const fetchSinTokenParams = (endpoint, data, options = {}, method = 'GET') => {
     return fetch(url, options);
   }
 };
-
-function queryParams(params) {
-  return Object.keys(params)
-    .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-    .join('&');
-}
 
 /**
  * Función para llamar a los servicios con el token
@@ -87,4 +81,47 @@ const fetchConToken = (endpoint, data, method = 'GET') => {
   }
 };
 
-export { fetchSinToken, fetchConToken, fetchSinTokenParams };
+/**
+ * Función para llamar a los servicios con los query params
+ * @param {*} endpoint
+ * @param {*} data
+ * @param {*} options ej {queryParams:{param1:'00213', param2: 'sdadas'}}
+ * @param {*} method
+ */
+const fetchConTokenParams = (endpoint, data, options = {}, method = 'GET') => {
+  let url = `${baseUrl}/${endpoint}`; // ej: localhost:4000/api/auth
+  const token = localStorage.getItem('token') || '';
+  options = {
+    method: method,
+    headers: {
+      'x-token': token,
+    },
+    body: JSON.stringify(data),
+    ...options,
+  };
+  if (options.queryParams) {
+    url +=
+      (url.indexOf('?') === -1 ? '?' : '&') + queryParams(options.queryParams);
+    delete options.queryParams;
+  }
+  if (method === 'GET') {
+    delete options.body;
+    return fetch(url);
+  } else {
+    options.headers?.push({ 'Content-type': 'application/json' });
+    return fetch(url, options);
+  }
+};
+
+function queryParams(params) {
+  return Object.keys(params)
+    .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+    .join('&');
+}
+
+export {
+  fetchSinToken,
+  fetchConToken,
+  fetchSinTokenParams,
+  fetchConTokenParams,
+};
